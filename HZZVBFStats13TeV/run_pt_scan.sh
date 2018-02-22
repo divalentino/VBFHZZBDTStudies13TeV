05d14bc5f0a9b7c877a7c0f500fd039c03feea3d
David Di Valentino <david.di.valentino@cern.ch>
2018-02-22 16:44:29 -0500

    Update

--- HZZVBFStats13TeV/run_pt_scan.sh
+++ HZZVBFStats13TeV/run_pt_scan.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-#for PT in 20 30 40 50 60 70; do
-for PT in $(seq 70 10 90); do
+for PT in 20 30 40 50 60 70; do
 
     echo "************ RUNNING PT SCAN FOR THRES. "${PT}" GeV ************"
     cat scripts/do_hzz_stats.sh | sed 's,pt_threshold=.*,pt_threshold='${PT}',g' &> test.sh
@@ -9,7 +8,4 @@ for PT in $(seq 70 10 90); do
 
     ./do_10_bin_calculation.sh &> lh_scan_result_pt_${PT}.out
 
-    # cat template-run_hzz_stats.sh | sed 's,MYCUT,'${PT}',g' &> scripts/run_hzz_stats.sh
-    # #./scripts/do_sig_scan.sh
-    # ./scripts/do_mu_scan.sh
 done
